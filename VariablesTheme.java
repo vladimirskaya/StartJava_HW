343368b09340c8bda5789d4b674e5eb900b3e754
Nina Vladimirskaya <naina148@mail.ru>
2022-10-14 21:26:12 +0300

    Added tasks 01 - 05

---
+++ VariablesTheme.java
@@ -0,0 +1,90 @@
+public class VariablesTheme {
+
+    public static void main(String arg[]){
+        /*System.out.println("\n1. Создание переменных и вывод их значений на консоль");
+        byte prosAmount = 1;
+        short systemType = 64;
+        int memoryCapacity = 8076;
+        long virtualMemoryCapacity = 14476;
+        float prosFrequency = 1.9f;
+        double display = 15.60;
+        char networkAdapters = '4';
+        System.out.println("Количество процессоров: " + prosAmount);
+        System.out.println("Тип системы: x" + systemType + "-based PC ");
+        System.out.println("Полный объем физической памяти: " + memoryCapacity + " МБ");
+        System.out.println("Виртуальная память: Макс. размер: " + virtualMemoryCapacity + " МБ");
+        System.out.println("Частота процессора: " + prosFrequency + " МГц");
+        System.out.println("Диагональ экрана: " + display + " дюймов");
+        System.out.println("Число сетевых адаптеров: " + networkAdapters);
+
+        System.out.println();
+        System.out.println("2. Расчет стоимости товара со скидкой");
+        double penPrice = 100, bookPrice = 200, discount = 13.5, allPriceWithDiscount;
+        float discountPercent = 11.0f;
+        allPriceWithDiscount = (penPrice + bookPrice) * (100 - discount) / 100;
+        discount = (penPrice + bookPrice) - allPriceWithDiscount;
+        System.out.println("Сумма скидки: " + discount + " рублей");
+        System.out.println("Общая сумма с учетом скидки: " + allPriceWithDiscount +" рублей");
+
+        System.out.println("\n3. Вывод на консоль слова JAVA");
+        char letterJ = 'J',
+             letterA = 'a',
+             letterVCap = 'V',
+             letterVLow = 'v';
+        System.out.println("    " + letterJ + "    " + letterA + "  " 
+                                    + letterVLow + "     " + letterVLow + "  " + letterA);
+        System.out.println("    " + letterJ + "   " + letterA + " " + letterA + "  "
+                                    + letterVLow + "   " + letterVLow + "  " 
+                                    + letterA + " " + letterA);
+        System.out.println(" " + letterJ + "  " + letterJ + "  "
+                                    + letterA + letterA + letterA + letterA + letterA + "  "
+                                    + letterVCap + " " + letterVCap + "  " 
+                                    + letterA + letterA + letterA + letterA + letterA);
+        System.out.println("  " + letterJ + letterJ + "  " + letterA + "     " + letterA + "  " 
+                                    + letterVCap + "  " + letterA + "     " + letterA); 
+
+
+       System.out.println("\n4. Отображение min и max значений числовых типов данных"); 
+       byte minByte = -128, maxByte = 127;
+       short minShort = -32_768, maxShort = 32_767;
+       int minInt = -2_147_483_648, maxInt = 2_147_483_647;
+       long minLong = -9_223_372_036_854_775_808L, maxLong = 9_223_372_036_854_775_807L;
+       System.out.println("\nПервоначальные значения переменных:  " + minByte + "    " + maxByte );
+       System.out.println("Значение после инкремента:           " + minByte++ + "    " + maxByte++);
+       System.out.println("Значение после декремента:           " + minByte-- + "    " + maxByte--);
+       System.out.println("\nПервоначальные значения переменных:  " + minShort + "    " + maxShort );
+       System.out.println("Значение после инкремента:           " + minShort++ + "    " + maxShort++);
+       System.out.println("Значение после декремента:           " + minShort-- + "    " + maxShort--);
+       System.out.println("\nПервоначальные значения переменных:  " + minInt + "    " + maxInt );
+       System.out.println("Значение после инкремента:           " + minInt++ + "    " + maxInt++);
+       System.out.println("Значение после декремента:           " + minInt-- + "    " + maxInt--);
+       System.out.println("\nПервоначальные значения переменных:  " + minLong + "    " + maxLong );
+       System.out.println("Значение после инкремента:           " + minLong++ + "    " + maxLong++);
+       System.out.println("Значение после декремента:           " + minLong-- + "    " + maxLong--); 
+
+       System.out.println("\nПерестановка значений переменных");
+       int a = 2, b = 5;
+       System.out.println("1. С помощью третьей переменной");
+       System.out.println("Исходные значение переменных: a = " + a + ", b = " + b);
+       int c = a;
+       a = b;
+       b = c;
+       System.out.println("Новые значение переменных:    a = " + a + ", b = " + b);
+       System.out.println("\n2. С помощью арифметических операций");
+       System.out.println("Исходные значение переменных: a = " + a + ", b = " + b);
+       a = a + b;
+       b = a - b;
+       a = a - b;
+       System.out.println("Новые значение переменных:    a = " + a + ", b = " + b);
+       System.out.println("\n3. С помощью побитовой операции");
+       System.out.println("Исходные значение переменных: a = " + a + ", b = " + b);
+       a = a ^ b;
+       b = a ^ b;
+       a = a ^ b;
+       System.out.println("Новые значение переменных:    a = " + a + ", b = " + b); */
+
+
+    }
+}
+
+
