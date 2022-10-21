8f48daab841676a4f944479caf8a29abc5a1c033
Nina Vladimirskaya <naina148@mail.ru>
2022-10-21 08:42:38 +0300

    Fixed

--- Lesson01/IfElseStatementTheme.java
+++ Lesson01/IfElseStatementTheme.java
@@ -1,17 +1,16 @@
 public class IfElseStatementTheme {
     
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         System.out.println("\n1. Перевод псевдокода на язык Java.");
         byte age = 56;
         float height = 1.70f;
-        boolean isMan = false;
-        char firstNameLetter;
+        boolean maleGender = false;
         if (age > 20) {
             System.out.println("Вам больше 20 лет.");
         } else {
             System.out.println("Вам меньше 20 лет");
         }    
-        if (!isMan) {
+        if (!maleGender) {
         System.out.println("Вы женщина.");
         } else {
             System.out.println("Вы мужчина.");
@@ -21,7 +20,7 @@ public class IfElseStatementTheme {
         } else {
             System.out.println("Ваш рост больше 1.80 м.");
         }
-        firstNameLetter = "mary".charAt(0);
+        char firstNameLetter = "mary".charAt(0);
         if (firstNameLetter == 'M') {
             System.out.println("Ваше имя начинается с буквы \"М\".");
         } else if (firstNameLetter == 'I') {
@@ -43,58 +42,58 @@ public class IfElseStatementTheme {
 
         System.out.println("\n3. Работа с числом.");
         int num = -9;
-        if (num != 0) {
-            if (num > 0) {
-                if (num % 2 == 0) {
+        if (num == 0) {
+            System.out.println("Число num = " + num + " равно нулю.");
+        } else {
+            if (num % 2 == 0) {
+                if (num > 0) {
                     System.out.println("Число num = " + num + " четное положительное.");
                 } else {
-                    System.out.println("Число num = " + num + " нечетное положительное.");
+                    System.out.println("Число num = " + num + " четное положительное.");
                 }
             } else {
-                if (num % 2 == 0) {
-                    System.out.println("Число num = " + num + " четное отрицательное.");
-                } else {
+                if (num > 0) {
+                    System.out.println("Число num = " + num + " нечетное положительное.");
+                } else{
                     System.out.println("Число num = " + num + " нечетное отрицательное.");
                 }
             }
-        } else {
-            System.out.println("Число num = " + num + " равно нулю.");
         }
 
         System.out.println("\n4.Поиск одинаковых цифр в числах");
         int num1 = 333;
         int num2 = 331;
-        int n = 0;
+        boolean isIdentical = false;
         System.out.println("Исходные числа: " + num1 + " и " + num2);
         int digit1 = num1 / 100;
         int digit2 = num2 / 100;
         if (digit1 == digit2) {
-            n++;
+            isIdentical = true;
             System.out.println("Числа имеют одинаковую цифру " + digit1 + " на 1 разряде.");
         }
         digit1 = num1 % 100 / 10;
         digit2 = num2 % 100 / 10;
         if (digit1 == digit2) {
-            n++;
+            isIdentical = true;
             System.out.println("Числа имеют одинаковую цифру " + digit1 + " на 2 разряде.");
         }
         digit1 = num1 % 10;
         digit2 = num2 % 10;
         if (digit1 == digit2) {
-            n++;
+            isIdentical = true;
             System.out.println("Числа имеют одинаковую цифру " + digit1 + " на 3 разряде.");
         }
-        if (n == 0) {
+        if (!isIdentical) {
             System.out.println("В данных числах нет одинаковых цифр на соответствующих разрядах.");
         }
 
         System.out.println("\n5. Определение буквы, числа или символа по их коду.");
         char symbol = '\u0057';
-        if (('\u0030' <= symbol) & (symbol <= '\u0039')) {
-            System.out.println("Значение " + symbol + " является цифрой.");
-        } else if (('\u0041' <= symbol) & (symbol <= '\u005A')) {
+        if ((symbol >= (char) '0') && (symbol <= (char) '9')) {
+            System.out.println("Значение " + symbol + " является цифрой."); 
+        } else if ((symbol >= (char) 'A') && (symbol <= (char) 'Z')) {
             System.out.println("Значение " + symbol + " является заглавной буквой.");
-        } else if (('\u0061' <= symbol) & (symbol <= '\u007A')) {
+        } else if ((symbol >= (char) 'a') && (symbol <= (char) 'z')) {
             System.out.println("Значение " + symbol + " прописной буквой.");
         } else {
             System.out.println("Значение " + symbol + " является ни буквой, ни цифрой.");
@@ -110,11 +109,11 @@ public class IfElseStatementTheme {
         } else {
             percent = 7f;
         }
-        double extraDeposit = deposit * percent / 100;
-        double allDeposit = deposit + extraDeposit;
+        double accruedInterest = deposit * percent / 100;
+        double totalSum = deposit + accruedInterest;
         System.out.println("Сумма вклада: " + deposit + ".");
-        System.out.println("Начисленный процент: " + extraDeposit);
-        System.out.println("Итоговая сумма:" + allDeposit);
+        System.out.println("Начисленный процент: " + accruedInterest);
+        System.out.println("Итоговая сумма: " + totalSum);
 
         System.out.println("\n7. Определение оценки по предметам.");
         int historyRate = 59;
@@ -141,16 +140,16 @@ public class IfElseStatementTheme {
         }
         System.out.println(historyBall + " - история");
         System.out.println(programmingBall + " - программирование");
-        System.out.println((historyBall + programmingBall)/2 + " - средний балл оценок по предметам");
-        System.out.println((historyRate + programmingRate)/2 + " - средний процент по предметам");
+        System.out.println((historyBall + programmingBall) / 2 + " - средний балл оценок по предметам");
+        System.out.println((historyRate + programmingRate) / 2 + " - средний процент по предметам");
 
         System.out.println("\n8. Расчет прибыли.");
         int renta = 5000;
         int sales = 13000;
         int cost = 9000;
         double annualProfit = (sales - renta - cost) * 12;
-        char flag = (annualProfit > 0) ? '+': ' ';
-        System.out.println("Годовая прибыль составляет: " + flag + annualProfit);
+        char plus  = (annualProfit > 0) ? '+': ' ';
+        System.out.println("Годовая прибыль составляет: " + plus  + annualProfit);
 
         System.out.println("\n9. Подсчет количества банкнот.");
         int needMoney = 567;
