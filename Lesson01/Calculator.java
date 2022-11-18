298cb07a6ac63faf5521ae044c576269760cbefd
Nina Vladimirskaya <naina148@mail.ru>
2022-11-18 13:56:09 +0300

    Fix 01 | Delete useless vars

--- Lesson01/Calculator.java
+++ Lesson01/Calculator.java
@@ -1,34 +1,29 @@
 public class Calculator {
 
     public static void main(String[] arg){
-        char plus = '+';
-        char minus = '-';
-        char multiply = '*';
-        char division = '/';
-        char rate = '^';
-        char remain = '%';
         int a = 9;
         int b = 6;
         int result = 1;
-        char sign = '/'; // задаем операцию, которая необходима пользователю
-        if (sign == plus) {
+        // задаем операцию, которая необходима пользователю
+        char sign = '/';
+        if (sign == '+') {
             result = a + b;
         }
-        if (sign == minus) {
+        if (sign == '-') {
             result = a - b;
         }
-        if (sign == multiply) {
+        if (sign == '*') {
             result = a * b;
         }
-        if (sign == division) {
+        if (sign == '/') {
             result = a / b;
         }
-        if (sign == rate) {
+        if (sign == '^') {
             for (int i = 0; i < b; i++) {
                 result *= a;
             }
         }
-        if (sign == remain) {
+        if (sign == '%') {
             result = a % b;
         }
         System.out.println(a + " " + sign + " " + b + " = " + result);
