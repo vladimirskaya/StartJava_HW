83a65cfb054b078751ba87addbd212b0e37da872
Nina Vladimirskaya <naina148@mail.ru>
2022-11-24 20:09:41 +0300

    HW part 01. Classes

--- Lesson02/Person.java
+++ Lesson02/Person.java
@@ -2,6 +2,6 @@ public class Person {
     char sex = 'm';
     String name = "Ivanov Ivan Ivanovich";
     float height = 1.78f;
-    float weight = 70.5;
+    float weight = 70.8;
     byte age = 25;
 }
