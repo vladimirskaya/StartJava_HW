98b8f5008765a40ffe8070dac13c49c090f23697
Nina Vladimirskaya <naina148@mail.ru>
2022-11-24 21:53:34 +0300

    HW part 01. Classes

--- Lesson02/Person.java
+++ Lesson02/Person.java
@@ -1,27 +1,22 @@
 public class Person {
-    char gender = 'm';
+    char gender = 'М';
     String name = "Петя Иванов";
     float height = 1.78f;
     float weight = 70.8f;
     byte age = 25;
 
     void go() {
-        System.out.println( name + " идет.");
     }
 
     void sit() {
-        System.out.println( name + " сидит.");
     }
 
     void run() {
-        System.out.println( name + " бежит.");
     }
 
     void say() {
-        System.out.println( name + " говорит.");
     }
 
     void learnJava() {
-        System.out.println( name + " учит Java.");
     }
 }
