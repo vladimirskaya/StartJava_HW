3eb9c4dc9746ab354eb088926c98fe9371ba6f6a
Nina Vladimirskaya <naina148@mail.ru>
2022-11-28 22:16:32 +0300

    fixed: 1. Calculator: added setSign, getSign, isSignCorrect | 2. CalculatorTest: fixed

--- Lesson02/Calculator.java
+++ Lesson02/Calculator.java
@@ -2,44 +2,59 @@ public class Calculator {
 
     int number1;
     int number2;
-    char sign;
     int result;
+    private char sign;
+    
 
-    public boolean isSignCorrect(char sign) {
+    public void setSign(char sign) {
         if ((sign == '+') || (sign == '-') || (sign == '*') || (sign == '/') || (sign == '^') 
                     || (sign == '%')) {
-            return true;
+            this.sign = sign;
+        } else {
+            this.sign = ' ';
+            System.out.println("Вы указали некорректный математический знак.");
         }
-        System.out.println("Вы указали некорректный математический знак.");
-        return false;
     }
 
-    public void calculate(char sign) {
-        if (isSignCorrect(sign)) {
-            this.sign = sign;
-            switch (this.sign) {
-            case '+' : 
-                result = number1 + number2;
-                break;
-            case '-' :
-                result = number1 - number2;
-                break;
-            case '*' :
-                result = number1 * number2;
-                break;
-            case '/' :
-                result = number1 / number2;
-                break;
-            case '^':
-                result = 1;
-                for (int i = 0; i < number2; i++) {
-                    result *= number1;
-                }
-                break;
-            case '%' :
-                result = number1 % number2;
-                 break;
+    public char getSign() {
+        if (isSignCorrect()) {
+            return sign;
+        }
+        return ' ';
+    }
+
+    public void calculate() {
+        if (isSignCorrect()) {
+            switch (sign) {
+                case '+' : 
+                    result = number1 + number2;
+                    break;
+                case '-' :
+                    result = number1 - number2;
+                    break;
+                case '*' :
+                    result = number1 * number2;
+                    break;
+                case '/' :
+                    result = number1 / number2;
+                    break;
+                case '^':
+                    result = 1;
+                    for (int i = 0; i < number2; i++) {
+                        result *= number1;
+                    }
+                    break;
+                case '%' :
+                    result = number1 % number2;
+                    break;
             }
         }
+    }
+    
+    private boolean isSignCorrect() {
+        if (!(this.sign == ' ')) {
+            return true;
+        }
+        return false;
     }
 }
--- Lesson02/CalculatorTest.java
+++ Lesson02/CalculatorTest.java
@@ -3,29 +3,27 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-
         String userAnswer;
         char sign;
         Calculator calc = new Calculator();
         Scanner scanner = new Scanner(System.in);
-        
+
         do {
             System.out.print("Введите первое число: ");
             calc.number1 = scanner.nextInt();
             do {
                 System.out.print("Введите знак математической операции: ");
-                String line = scanner.next();
-                sign = line.charAt(0);
-            } while (!calc.isSignCorrect(sign));
+                calc.setSign(scanner.next().charAt(0));
+            } while (calc.getSign() == ' ');
             System.out.print("Введите второе число: ");
             calc.number2 = scanner.nextInt();
-            calc.calculate(sign);
-            System.out.println(calc.number1 + " " + sign + " " + calc.number2 + " = " + calc.result);
+            calc.calculate();
+            System.out.println(calc.number1 + " " + calc.getSign() + " " + calc.number2 + " = " + calc.result);
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]: ");
                 userAnswer = scanner.next();
-            } while (!((userAnswer.compareToIgnoreCase("yes") == 0) || (userAnswer.compareToIgnoreCase("no") == 0)));
-        } while (userAnswer.compareToIgnoreCase("yes") == 0);
+            } while (!(("yes".equals(userAnswer)) || ("no".equals(userAnswer))));
+        } while ("yes".equals(userAnswer));
        
     }
 }
