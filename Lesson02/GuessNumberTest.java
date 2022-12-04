38f591c4d2247356b064458fdb88a3395442922a
Nina Vladimirskaya <naina148@mail.ru>
2022-12-04 06:45:13 +0300

    fix: 1. added question about contitueing

--- Lesson02/GuessNumberTest.java
+++ Lesson02/GuessNumberTest.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class GuessNumberTest {
     
     public static void main(String[] args) {
+        boolean isEnded = false;
+        String answerForContinue;
         Scanner scanner = new Scanner(System.in);
         System.out.print("\nВведите имя первого игрока: ");
         Player player1 = new Player(scanner.next());
@@ -10,7 +12,20 @@ public class GuessNumberTest {
         Player player2 = new Player(scanner.next());
         GuessNumber game = new GuessNumber(player1, player2);
         do {
-            game.playGame();
-        } while (!game.getIsEnded());
+            game.play();
+            System.out.println("Хотите продолжить игру? [yes/no]: ");
+            // Scanner scanner = new Scanner(System.in);
+            answerForContinue = scanner.next();
+            if ("yes".equals(answerForContinue)) {
+                isEnded = false;;
+            } else if ("no".equals(answerForContinue)) {
+                isEnded  = true;
+            } else {
+                do {
+                    System.out.println("Хотите продолжить игру? [yes/no]: ");
+                    answerForContinue = scanner.next();
+                    } while (!(("yes".equals(answerForContinue)) || ("no".equals(answerForContinue))));
+            }
+        } while (!isEnded);
     }
 }
