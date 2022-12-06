2ffc07ef98752230fed73cf4a9999c74746b62fa
Nina Vladimirskaya <naina148@mail.ru>
2022-12-06 13:42:30 +0300

    fix: 1.add empty string; 2. del empty string; 3. del .toString()

--- Lesson02/JaegerTest.java
+++ Lesson02/JaegerTest.java
@@ -9,23 +9,23 @@ public class JaegerTest {
         jaeger1.setSpeed(8);
         jaeger1.setStrength(7);
         jaeger1.setArmor(3);
+
         Jaeger jaeger2 = new Jaeger("Cherno Alpha", "Russia", 85.34f, 2.412, 3, 10, 10);
 
-        System.out.println("\n" + jaeger1.toString());
-        System.out.println(jaeger2.toString());
+        System.out.println("\n" + jaeger1);
+        System.out.println(jaeger2);
         jaeger1.battle(jaeger2);
 
         jaeger1.setName("Black Mamba");
         jaeger1.setOrigin("India");
-        System.out.println("\n" + jaeger1.toString());
+        System.out.println("\n" + jaeger1);
         jaeger1.move();
         jaeger1.fight();
         System.out.println("\nNew robot's generation: " + jaeger2.getName());
         jaeger2.setName("Anatoliy 2053");
         jaeger2.setSpeed(53);
         jaeger2.setArmor(100);
-        System.out.println(jaeger2.toString());
+        System.out.println(jaeger2);
         jaeger2.battle(jaeger1);
-
     }
 }
