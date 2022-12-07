3121451c02491dad947479dc44a895c42495ed14
Nina Vladimirskaya <naina148@mail.ru>
2022-12-07 19:28:54 +0300

    fix: 1.rename obj -> player; 2. compareNumber() return boolean; 3. correct private classes; 4. correct classes's queue; 5. rename createNewGame -> init, insertNumber -> inputNumber; 6. correct random

--- Lesson02/GuessNumber.java
+++ Lesson02/GuessNumber.java
@@ -6,64 +6,64 @@ public class GuessNumber {
     private static Player player2;
     private static Player currentPlayer;
     private static int secretNumber;
-    private static boolean isGuessed;
 
-    public GuessNumber(Player obj1, Player obj2) {
-        player1 = obj1;
-        player2 = obj2;
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
         currentPlayer = player1;
     }
 
-    public void createNewGame() {
+    
+
+    public void play() {
+        init();
+        boolean result;
+        do {
+            inputNumber(currentPlayer);
+            result = compareNumber(currentPlayer);
+            if (!result) {
+                changePlayer(currentPlayer);
+            }
+        } while (!result);
+    }
+
+    private void init() {
         Random random = new Random();
-        secretNumber = random.nextInt(100);
+        secretNumber = random.nextInt(100) + 1;
         System.out.println("Случайное число = " + secretNumber);
-        isGuessed = false;
         currentPlayer = player1;
         System.out.println("Число отгадывает " + currentPlayer.getName());
     }
 
-    public void play() {
-        createNewGame();
+    private static void inputNumber(Player currentPlayer) {
+        Scanner scanner = new Scanner(System.in);
         do {
-            insertNumber(currentPlayer);
-            compareNumber(currentPlayer);
-            if (!isGuessed) {
-                changePlayers(currentPlayer);
-            }
-        } while (!isGuessed);
+            System.out.println("Введите число: ");
+            int number = scanner.nextInt();
+            currentPlayer.setNumber(number);
+        } while (currentPlayer.getNumber() == 0);
     }
 
-    public static void changePlayers(Player player) {
+    private static boolean compareNumber(Player player) {
+        if (player.getNumber() == secretNumber) {
+            System.out.println(player.getName() + " угадал число!");
+            return true;
+        } else {
+            if (player.getNumber() > secretNumber) {
+                    System.out.println("Число " + player.getNumber() + " больше того, что загадал компьютер.");
+            } else {
+                System.out.println("Число " + player.getNumber() + " меньше того, что загадал компьютер.");
+            }
+            return false;
+        }
+    }
+
+    private static void changePlayer(Player player) {
         if (player == player1) {
             currentPlayer = player2;
         } else {
             currentPlayer = player1;
         }
         System.out.println("Число отгадывает " + currentPlayer.getName());
-        
-    }
-
-    public static void insertNumber(Player currentPlayer) {
-        Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("Введите число: ");
-            int number = scanner.nextInt();
-            currentPlayer.setNumber(number);
-        } while (currentPlayer.getNumber() == -1);
-    }
-
-    public static void compareNumber(Player player) {
-        if (player.getNumber() == secretNumber) {
-            System.out.println(player.getName() + " угадал число!");
-            isGuessed = true;
-        } else {
-            isGuessed = false;
-            if (player.getNumber() > secretNumber) {
-                    System.out.println("Число " + player.getNumber() + " больше того, что загадал компьютер.");
-            } else {
-                System.out.println("Число " + player.getNumber() + " меньше того, что загадал компьютер.");
-            }
-        }
     }
 }
