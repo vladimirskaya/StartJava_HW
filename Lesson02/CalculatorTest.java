44c82614d6c96ac670348bbaa2bb3fd1a1ec57c9
Nina Vladimirskaya <naina148@mail.ru>
2022-12-10 08:03:47 +0300

    fix: 1. modify main()

--- Lesson02/CalculatorTest.java
+++ Lesson02/CalculatorTest.java
@@ -11,22 +11,20 @@ public class CalculatorTest {
         do {
             do {
                 System.out.print("Введите первое число: ");
-                calc.setNumber1(scanner.nextInt());
-            } while (calc.getNumber1() == -1);
+            } while (!calc.setNumber1(scanner.nextInt()));
             do {
                 System.out.print("Введите знак математической операции: ");
-                calc.setSign(scanner.next().charAt(0));
-            } while (calc.getSign() == '\u0000');
+            } while (!calc.setSign(scanner.next().charAt(0)));
             do {
                 System.out.print("Введите второе число: ");
-                calc.setNumber2(scanner.nextInt());
-            } while (calc.getNumber2() == -1);
+            } while (!calc.setNumber2(scanner.nextInt()));
             calc.calculate();
-            System.out.println(calc.getNumber1() + " " + calc.getSign() + " " + calc.getNumber2() + " = " + calc.getResult());
+            System.out.println(calc.getNumber1() + " " + calc.getSign() + " " 
+                    + calc.getNumber2() + " = " + calc.getResult());
             do {
                 System.out.println("Хотите продолжить вычисления? [yes/no]: ");
                 userAnswer = scanner.next();
-            } while (!(("yes".equals(userAnswer)) || ("no".equals(userAnswer))));
+            } while ((!"yes".equals(userAnswer)) && (!"no".equals(userAnswer)));
         } while ("yes".equals(userAnswer));
        
     }
