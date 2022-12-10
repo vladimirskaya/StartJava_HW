6d08bc4a8b454c2de99c91e9e030220522a857d3
Nina Vladimirskaya <naina148@mail.ru>
2022-12-10 08:01:35 +0300

    fix: 1. getX() returns boolean   2. del sign checking

--- Lesson02/Calculator.java
+++ Lesson02/Calculator.java
@@ -5,74 +5,69 @@ public class Calculator {
     private int result;
     private char sign;
     
-    public void setNumber1(int number) {
+    public boolean setNumber1(int number) {
         if (isNumberCorrect(number)) {
             number1 = number;
-        } else {
-            System.out.println("Укажите положительное целое число.");
-            number1 = -1;
+            return true;
         }
+        System.out.println("Укажите положительное целое число.");
+        return false;
     }
 
     public int getNumber1() {
         return number1;
     }
 
-    public void setNumber2(int number) {
+    public boolean setNumber2(int number) {
         if (isNumberCorrect(number)) {
             number2 = number;
-        } else {
-            System.out.println("Укажите положительное целое число.");
-            number2 = -1;
+            return true;
         }
+        System.out.println("Укажите положительное целое число.");
+        return false;
     }
 
     public int getNumber2() {
         return number2;
     }
 
-    public void setSign(char sign) {
-        if ((sign == '+') || (sign == '-') || (sign == '*') || (sign == '/') || (sign == '^') 
-                    || (sign == '%')) {
+    public boolean setSign(char sign) {
+        if ((sign == '+') || (sign == '-') || (sign == '*') || 
+                (sign == '/') || (sign == '^') || (sign == '%')) {
             this.sign = sign;
-        } else {
-            this.sign = '\u0000';
-            System.out.println("Вы указали некорректный математический знак.");
+            return true;
         }
+        System.out.println("Вы указали некорректный математический знак.");
+        return false;
     }
 
     public char getSign() {
-        if (isSignCorrect()) {
-            return sign;
-        }
-        return ' ';
+        return sign;
     }
 
     public void calculate() {
-        if (isSignCorrect()) {
-            switch (sign) {
-                case '+' : 
-                    result = number1 + number2;
-                    break;
-                case '-' :
-                    result = number1 - number2;
-                    break;
-                case '*' :
-                    result = number1 * number2;
-                    break;
-                case '/' :
-                    result = number1 / number2;
-                    break;
-                case '^':
-                    result = 1;
-                    for (int i = 0; i < number2; i++) {
-                        result *= number1;
-                    }
-                    break;
-                case '%' :
-                    result = number1 % number2;
-                    break;
-            }
+        switch (sign) {
+            case '+' : 
+                result = number1 + number2;
+                break;
+            case '-' :
+                result = number1 - number2;
+                break;
+            case '*' :
+                result = number1 * number2;
+                break;
+            case '/' :
+                result = number1 / number2;
+                break;
+            case '^':
+                result = 1;
+                for (int i = 0; i < number2; i++) {
+                    result *= number1;
+                }
+                break;
+            case '%' :
+                result = number1 % number2;
+                break;
         }
     }
 
@@ -81,16 +76,6 @@ public class Calculator {
     }
     
     private boolean isNumberCorrect(int x) {
-        if (x > 0) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean isSignCorrect() {
-        if (!(this.sign == ' ')) {
-            return true;
-        }
-        return false;
+        return x > 0 ? true : false;
     }
 }
